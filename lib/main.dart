6e28e2b38033067edf54e20bb741644727be47ce
anmol <anmolgupta02@gmail.com>
2020-07-29 02:22:57 +0530

    Added Expanded Widget and Stretch. UI Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,52 +12,76 @@ class XyloPhoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  playSound(1);
-                }),
-            FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  playSound(2);
-                }),
-            FlatButton(
-                color: Colors.yellowAccent,
-                onPressed: () {
-                  playSound(3);
-                }),
-            FlatButton(
-                color: Colors.green,
-                onPressed: () {
-                  playSound(4);
-                }),
-            FlatButton(
-                color: Colors.teal,
-                onPressed: () {
-                  playSound(5);
-                }),
-            FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  playSound(6);
-                }),
-            FlatButton(
-                color: Colors.purple,
-                onPressed: () {
-                  playSound(7);
-                }),
+            Expanded(
+              child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    playSound(1);
+                  }),
+            ),
+            Expanded(
+              child: FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playSound(2);
+                  }),
+            ),
+            Expanded(
+              child: FlatButton(
+                  color: Colors.yellowAccent,
+                  onPressed: () {
+                    playSound(3);
+                  }),
+            ),
+            Expanded(
+              child: FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    playSound(4);
+                  }),
+            ),
+            Expanded(
+              child: FlatButton(
+                  color: Colors.teal,
+                  onPressed: () {
+                    playSound(5);
+                  }),
+            ),
+            Expanded(
+              child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    playSound(6);
+                  }),
+            ),
+            Expanded(
+              child: FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    playSound(7);
+                  }),
+            ),
           ],
         )),
       ),
     );
   }
-
   void playSound(int num) {
     final player = AudioCache();
     player.play('note$num.wav');
   }
+
+// Another Method to reduce code Duplicacy.
+//  Widget custom_Xylo(int num, Color color){
+//    return FlatButton(
+//      color: color;
+//
+//    )
+//  }
+
 }
